ffd2ed4d215b825288cdca66a2e582e21bd82932
Ben Neilsen <ben@neil.sn>
2026-06-07 23:37:56 -0400

    add titlecard

---
+++ src/title.h
@@ -0,0 +1,8 @@
+#ifndef TITLE_H
+#define TITLE_H
+
+#define TITLE_WEBSITE_ATTRIBUTION "ASCII Art Generator by https://patorjk.com"
+
+int get_titlecard (const char **title, const char **attr);
+
+#endif
